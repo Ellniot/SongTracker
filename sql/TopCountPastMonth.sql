27bcbd7918a548af865847c3702182833d39d705
Ellot Carson <carselli.c@gmail.com>
2021-08-12 21:32:54 -0500

    minor sql tweak

--- sql/TopCountPastMonth.sql
+++ sql/TopCountPastMonth.sql
@@ -1,5 +1,9 @@
+--- Seems to only count the ones in the last month... not sure tho ---
 SELECT song_name, artist_name, COUNT(song_name||artist_name) AS cnt
 FROM the_current_test
+WHERE timestamp > (
+	SELECT date('now','+1 month')
+)
 GROUP BY song_name
 ORDER BY cnt DESC 
 LIMIT 10
